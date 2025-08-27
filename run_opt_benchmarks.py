8ac9d0608f7c9c4b2cd7ec0ae416baac65cdbfd5
Adam Geller <adam_geller@msn.com>
2025-08-27 20:41:14 +0000

    Fix raw data fields
    Signed-off-by: Adam Geller <adam_geller@msn.com>

--- run_opt_benchmarks.py
+++ run_opt_benchmarks.py
@@ -225,7 +225,7 @@ if __name__ == '__main__':
         print("Outputting raw data to " + args.raw_data_file)
         raw_file = open(args.raw_data_file, 'w')
         raw_file.write(
-            "template, seed, block len, rz weight, n qubits, launch mean, launch sem, launch opt mean, launch opt sem, jit mean, jit sem, jit opt mean, jit opt sem, # rzs, opt # rzs\n"
+            "template, seed, block len, rz weight, n qubits, launch, launch opt, jit, jit opt, # rzs, opt # rzs\n"
         )
         raw_file.flush()
     for target in args.targets:
